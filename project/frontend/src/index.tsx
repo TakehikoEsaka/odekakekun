a19369882266c54fd34ab75f5995cac96bd475f2
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-02-19 20:55:50 +0900

    react18にupgrade

--- project/frontend/src/index.tsx
+++ project/frontend/src/index.tsx
@@ -1,14 +1,22 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { RecoilRoot } from "recoil";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../src/styles/style";
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <ChakraProvider theme={theme}>
     <RecoilRoot>
       <App />
     </RecoilRoot>
-  </ChakraProvider>,
-  document.getElementById("root")
+  </ChakraProvider>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
