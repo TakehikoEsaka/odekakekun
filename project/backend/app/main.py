2953c4de9e99056a30ce9fb0de4064685eb59ce0
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-02-22 10:00:42 +0900

    fastAPIのクエリパラメータを作成

--- project/backend/app/main.py
+++ project/backend/app/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+import requests as re
+import random
 
 app = FastAPI()
 
 @app.get("/ping")
 def pong():
-    return {"Hello": "world!"}
+    print('health endpoint')
 
 @app.get("/suggest")
-def suggest():
-    
+def suggest(place = None, way = None, time = None, id : int = 10):
+    if place == None or way == None or time == None:
+        return False
+    else:
+        # res = chatGPT()
+        res = re.get("https://jsonplaceholder.typicode.com/todos/").json()[id]["title"]
+    return res
+
+if __name__ == "__main__":
+    print(suggest())
