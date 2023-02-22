661bed32b103d0deacb37a147541c21fad83d9d1
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-02-22 10:01:44 +0900

    todo追加

--- project/frontend/src/hooks/useSuggest.tsx
+++ project/frontend/src/hooks/useSuggest.tsx
@@ -19,6 +19,8 @@ export const useSuggest = (): SuggestHookReturnType => {
   const [suggest, setSuggest] = useState<string>("Smple Suggest from Chat-GPT");
 
   const getSuggest = (wishVariables : string) => {
+    // fastAPIで出てきたリクエスト内容
+    // http://localhost:8000/suggest?place=%E9%AB%98%E5%86%86%E5%AF%BA&way=%E5%BE%92%E6%AD%A9&time=1%E6%99%82%E9%96%93&id=11
     axios
       .get("http://odekakekun-backend_container/suggest&query=test")
       // Axiosのresponceの型はAxiosResponse型をimportして使う
