1381f5ea9bb1d9d37b3b4b9819e428154eba59cd
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-02-23 20:59:51 +0900

    routingを追加

--- project/frontend/src/index.tsx
+++ project/frontend/src/index.tsx
@@ -1,16 +1,38 @@
 import ReactDOM from "react-dom";
-import { App } from "./App";
+import { Home } from "./pages/Home";
+
 import { RecoilRoot } from "recoil";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../src/styles/style";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
+
+import { NotFound } from "./pages/NotFound";
+
+import { Route, BrowserRouter, Routes, Link } from "react-router-dom";
 
 ReactDOM.render(
-  <ChakraProvider theme={theme}>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
-  </ChakraProvider>,
+  <BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <RecoilRoot>
+        <nav>
+          <ul>
+            <li>
+              <Link to="login">Login</Link>
+            </li>
+            <li>
+              <Link to="home">Home</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="login" element={<div />} />
+          <Route path="home" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </RecoilRoot>
+    </ChakraProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
