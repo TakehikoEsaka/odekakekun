fed566808bcdb287b5dc9f62139a00d54d52b8bc
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-03-05 18:11:04 +0900

    axiosを使ってbackendにリクエストを送る

--- project/frontend/src/pages/Login.jsx
+++ project/frontend/src/pages/Login.jsx
@@ -7,31 +7,49 @@ import {
   FormLabel,
   Input,
   Stack,
-  Flex,
   Container,
 } from "@chakra-ui/react";
+import axios, { AxiosResponse } from "axios";
 
 export const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("sfa@gmail.com");
+  const [password, setPassword] = useState("fasd");
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [error, setError] = useState("defaut error message");
 
-  //   const handleSubmit = (e) => {
-  //     event.preventDefault();
-  //     setIsLoading(true);
+  // const formData = new FormData();
+  // // OAuth2 expects form data, not JSON data
+  // formData.append("username", email);
+  // formData.append("password", password);
 
-  //     // TODO: ログイン処理を実装
+  const handleSubmit = async (event) => {
+    console.log("hi");
+    event.preventDefault();
+    setIsLoading(true);
 
-  //     setIsLoading(false);
-  //   };
+    const formData = new FormData();
+    formData.append("username", email);
+    formData.append("password", password);
+
+    // ここで投げるLoginの型が違うのか認証が通らない
+    axios
+      .post("http://localhost:80/token", formData)
+      .then((response) => {
+        // 成功時の処理
+        console.log(response);
+      })
+      .catch((error) => {
+        // エラー時の処理
+        console.log(error);
+      });
+
+    setIsLoading(false);
+  };
 
   return (
     <Container display="flex" justifyContent="center" alignItems="center">
       <Box>
-        <form
-        //   onSubmit={handleSubmit}
-        >
+        <form onSubmit={handleSubmit}>
           <Stack spacing={3}>
             <FormControl isRequired>
               <FormLabel>Email</FormLabel>
@@ -55,6 +73,9 @@ export const Login = () => {
               <FormErrorMessage>{error}</FormErrorMessage>
             </FormControl>
 
+            {error && <p>{error}</p>}
+            <p>{email}</p>
+            <p>{password}</p>
             <Button
               type="submit"
               colorScheme="blue"
