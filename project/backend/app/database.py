6e26d3482deb60d89988ede5bb899189d6992cb9
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-03-05 16:23:59 +0900

    alembicの用意

--- project/backend/app/database.py
+++ project/backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./user.db"
+DATABASE_URL = "sqlite:///./db/user.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread" : False})
 
