0a5561935e3f895f99036d27506d0ecab844e8d5
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-03-11 14:08:39 +0900

    最新の10件だけhistoriesに表示するようにする

--- project/backend/app/routes/suggest.py
+++ project/backend/app/routes/suggest.py
@@ -25,23 +25,27 @@ def suggest(question : str, current_user: models.UserInfo = Depends(oauth2.get_c
     # ここでanswerをchat-gptからget
     answer = "".join(random.choice(string.ascii_lowercase) for i in range(10))
 
+    print("starts")
     # ログインしている時はDBに追加・そうでない時は追加しない
     if current_user:
+        print("youre logined")
         new_suggest = models.Suggest(user_id = current_user.user_id, question = question, answer = answer)
         db.add(new_suggest)
         db.commit()
         db.refresh(new_suggest)
+        print("put into DB")
 
     return answer
 
 @router.get("/get_all_suggest", tags = ["suggest"])
 def get_suggest(current_user: models.UserInfo = Depends(oauth2.get_current_active_user), db: Session = Depends(get_db)):
-    
-    
-    user = db.query(models.UserInfo).filter(models.UserInfo.user_id == current_user.user_id).first()
-    
-    df = pd.DataFrame(columns=["question", "answer"])
-    for s in user.suggestions:
-        df = df.append({"question": s.question, "answer": s.answer}, ignore_index=True)
 
+    user = db.query(models.UserInfo).filter(models.UserInfo.user_id == current_user.user_id).first()
+    print(user)
+
+    df = pd.DataFrame(columns=["question", "answer"])    
+    for s in user.suggestions[-1:-10:-1]:
+        df = pd.concat([df, pd.DataFrame([{"question": s.question, "answer": s.answer}])], ignore_index=True)
+
+    print(df.shape)
     return df.to_dict(orient="records")
