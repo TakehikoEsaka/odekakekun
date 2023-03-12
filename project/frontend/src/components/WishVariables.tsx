439487723150194326e9141a19026bdb28890895
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-03-12 17:31:34 +0900

    react-hook-formに対応

--- project/frontend/src/components/WishVariables.tsx
+++ project/frontend/src/components/WishVariables.tsx
@@ -9,6 +9,7 @@ import {
   Select,
 } from "@chakra-ui/react";
 // import Select from "../hooks/ChakraReactSelect";
+import { useForm } from "react-hook-form";
 
 interface OPtions {
   value: string;
@@ -16,7 +17,7 @@ interface OPtions {
 }
 
 interface FormValues {
-  Place: string;
+  place: string;
   hour: string;
   way: string;
 }
@@ -40,69 +41,96 @@ type PropType = {
 };
 
 export const WishVariables = (props: PropType) => {
-  const trySuggest = () => {
+  const { register, handleSubmit } = useForm<FormValues>();
+
+  const trySuggest = (data: FormValues) => {
     // ここでreact-hook-formで染めれてないからちょっと辛い。。
-    var question = "近くにある徒歩を使って使って1時間でいける場所を探す";
+
+    const { place, hour, way } = data;
+    console.log("data is ", data);
+    console.log("hour is ", hour);
+    const question = `${place}から${hour}以内で${way}を使っていけるおすすめの場所を探してください`;
+    console.log(question);
     props.getSuggest(question);
   };
 
   return (
     <>
-      <Grid
-        // ここはレスポンシブで列の数を変えておく対応させておく
-        templateColumns={{ base: "repeat(1, 1fr)", lg: "repeat(3, 1fr)" }}
-      >
-        <FormControl p={4}>
-          <FormLabel>場所</FormLabel>
-          <Grid templateColumns="repeat(5, 1fr)" gap={4} alignItems="center">
-            <GridItem colSpan={4}>
-              <Input placeholder="地名/駅名" defaultValue="高円寺" />
-            </GridItem>
-            <GridItem colSpan={1} color="blackAlpha.700">
-              から
-            </GridItem>
-          </Grid>
-        </FormControl>
-
-        {/* 選択時にiphoneのキーボードが出ないようにしたい。inputProps={{ readOnly: true }}をつける必要があるけど難しいな。。*/}
-        <FormControl p={3} isReadOnly={true}>
-          <FormLabel>時間</FormLabel>
-          <Grid templateColumns="repeat(5, 1fr)" gap={4} alignItems="center">
-            <GridItem colSpan={4}>
-              <Select placeholder="30分">
-                {hourOptions.map((option) => (
-                  <option value={option.label}>{option.label}</option>
-                ))}
-              </Select>
-            </GridItem>
-            <GridItem colSpan={1}>以内</GridItem>
-          </Grid>
-        </FormControl>
-
-        <FormControl p={3}>
-          <FormLabel>交通手段</FormLabel>
-          <Grid templateColumns="repeat(5, 1fr)" gap={4} alignItems="center">
-            <GridItem colSpan={4}>
-              <Select placeholder="自転車">
-                {wayOptions.map((option) => (
-                  <option value={option.label}>{option.label}</option>
-                ))}
-              </Select>
-            </GridItem>
-            <GridItem colSpan={1}>でいける</GridItem>
-          </Grid>
-        </FormControl>
-      </Grid>
-
-      <Flex justifyContent="center">
-        <Button
-          // chakraUIならcolorSchemeを指定すると背景色と背景色と対応する色味をセットでもってくれてるのでデザイン知識がなくても補完してくれるs
-          colorScheme="blue"
-          onClick={trySuggest}
+      <form onSubmit={handleSubmit(trySuggest)}>
+        <Grid
+          // ここはレスポンシブで列の数を変えておく対応させておく
+          templateColumns={{ base: "repeat(1, 1fr)", lg: "repeat(3, 1fr)" }}
         >
-          おすすめの場所を探す
-        </Button>
-      </Flex>
+          <FormControl p={4}>
+            <FormLabel>場所</FormLabel>
+            <Grid templateColumns="repeat(5, 1fr)" gap={4} alignItems="center">
+              <GridItem colSpan={4}>
+                <Input
+                  placeholder="地名/駅名"
+                  defaultValue="高円寺"
+                  {...register("place")}
+                />
+              </GridItem>
+              <GridItem colSpan={1} color="blackAlpha.700">
+                から
+              </GridItem>
+            </Grid>
+          </FormControl>
+
+          {/* 選択時にiphoneのキーボードが出ないようにしたい。inputProps={{ readOnly: true }}をつける必要があるけど難しいな。。*/}
+          <FormControl p={3} isReadOnly={true}>
+            <FormLabel>時間</FormLabel>
+            <Grid templateColumns="repeat(5, 1fr)" gap={4} alignItems="center">
+              <GridItem colSpan={4}>
+                <Select
+                  placeholder="30分"
+                  defaultValue="30分"
+                  {...register("hour")}
+                >
+                  {hourOptions.map((option) => (
+                    <option key={option.label} value={option.label}>
+                      {option.label}
+                    </option>
+                  ))}
+                </Select>
+              </GridItem>
+              <GridItem colSpan={1}>以内</GridItem>
+            </Grid>
+          </FormControl>
+
+          <FormControl p={3}>
+            <FormLabel>交通手段</FormLabel>
+            <Grid templateColumns="repeat(5, 1fr)" gap={4} alignItems="center">
+              <GridItem colSpan={4}>
+                <Select
+                  placeholder="自転車"
+                  // react-hook-formにdefaultのvalueを設定しておく必要がある
+                  defaultValue="自転車"
+                  {...register("way")}
+                >
+                  {wayOptions.map((option) => (
+                    <option key={option.label} value={option.label}>
+                      {option.label}
+                    </option>
+                  ))}
+                </Select>
+              </GridItem>
+              <GridItem colSpan={1}>でいける</GridItem>
+            </Grid>
+          </FormControl>
+        </Grid>
+
+        <Flex justifyContent="center">
+          <Button
+            // chakraUIならcolorSchemeを指定すると背景色と背景色と対応する色味をセットでもってくれてるのでデザイン知識がなくても補完してくれるs
+            colorScheme="blue"
+            // typeをつけることでフォームの送信をする事ができる
+            type="submit"
+          >
+            おすすめの場所を探す
+          </Button>
+        </Flex>
+      </form>
     </>
   );
 };
