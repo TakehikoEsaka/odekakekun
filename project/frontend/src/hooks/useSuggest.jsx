9825b6cad05b66e34b0ee61e36a79e07d3fb883f
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-05-18 19:39:30 +0900

    Chat-GPTのリクエストにタイムアウトを設定

--- project/frontend/src/hooks/useSuggest.jsx
+++ project/frontend/src/hooks/useSuggest.jsx
@@ -23,6 +23,7 @@ export const useSuggest = () => {
           Authorization: `Bearer ${token}`,
           accept: "application/json",
         },
+        timeout: 60000, // タイムアウト時間をミリ秒で指定
       })
       .then((res) => {
         console.log("res.data is ", res.data);
@@ -41,12 +42,24 @@ export const useSuggest = () => {
           //TODO レスポンスの型定義をつけておく。もし違うレスポンスが帰ってきた時ようにエラー文言をつける
           //TODO 次のエラーが起こったらhooksの処理を中断する。openai.error.APIConnectionError: Error communicating with OpenAI: ('Connection aborted.', OSError(65, 'No route to host')
           //ASK。以下でsetSuggest(...suggest, res.data)がダメな理由
-          setSuggest(res.data);
+          // TODO ここをnullにするのか？一度エラーになって再度復活できる時にメッセージを更新する方法を調べる
+          setSuggest({ message: null, ...res.data });
         }
       })
       .catch((error) => {
-        // 例外が発生した場合の処理
-        console.error("There was a problem with the fetch operation:", error);
+        // その他のエラーの場合の処理
+        console.log("エラーが発生しました", error.message);
+        if (error.code === "ECONNABORTED") {
+          // タイムアウトエラーの場合の処理
+          console.log("リクエストがタイムアウトしました");
+          setSuggest({
+            message:
+              "ネットワークトラブルのため回答が出せませんでした。もう一度やり直すか暫くたってから再度お試し下さい。",
+            suggest_place: {},
+            suggest_description: {},
+            suggest_distance: {},
+          });
+        }
       });
   };
 
