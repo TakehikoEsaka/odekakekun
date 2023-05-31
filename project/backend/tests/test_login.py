79794cf3fae185a8f7315a21da1003704ee0613b
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-05-31 09:25:20 +0900

    loginとsuggest周りのテスト追加

--- project/backend/tests/test_login.py
+++ project/backend/tests/test_login.py
@@ -1,10 +1,9 @@
 EMAIL = "example6@example.com"
 PASSWORD = "chimichangas4life"
 
-# test_db: Sessionを引数にすることで事前定義したfixtureを使用する事ができる
+# fixtureで定義したtest_client関数を引数に指定することで紐付けが可能
 def test_create_user(test_client):
 
-    # OAuth2PasswordRequestFormで受け取るAPIなので、jsonパラメータではなくてdataパラメータを使う
     response = test_client.post(
         "/create_user/",
         data={"username": EMAIL, "password": PASSWORD},
@@ -12,3 +11,29 @@ def test_create_user(test_client):
     assert response.status_code == 200, response.text
     data = response.json()
     assert data["username"] == EMAIL
+
+def test_get_token(test_client):
+
+    response = test_client.post(
+        "/token/",
+        data={"username": EMAIL, "password": PASSWORD},
+    )
+    assert response.status_code == 200, response.text
+
+    TOKEN = response.json()["access_token"]
+
+def test_session_check(test_client):
+
+    # TODO test関数は上から順番に実行されているみたいだが、1つ前のテストの値を再利用する事はできないのか
+    token = test_client.post(
+        "/token/",
+        data={"username": EMAIL, "password": PASSWORD},
+    ).json()["access_token"]
+
+    response = test_client.get(
+        "/login/session-check/",
+        headers = {
+            "Authorization": f"Bearer {token}"
+        }
+    )
+    assert response.status_code == 200, response.text
---
+++ project/backend/tests/test_suggest.py
@@ -0,0 +1,56 @@
+EMAIL = "example6@example.com"
+PASSWORD = "chimichangas4life"
+
+PLACE = "出町柳"
+TIME = "30分"
+WAY = "自転車"
+
+# TODO ここにpytestの時間待機の文を入れる
+def test_suggest(test_client):
+
+    # ログイン処理の共通化をどうするか考える
+    response = test_client.post(
+        "/create_user/",
+        data={"username": EMAIL, "password": PASSWORD},
+    )
+    assert response.status_code == 200, response.text
+
+    response = test_client.post(
+        "/token/",
+        data={"username": EMAIL, "password": PASSWORD},
+    )
+    assert response.status_code == 200, response.text
+    token = response.json()["access_token"]  
+
+    response = test_client.post(
+        "/suggest/",
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {token}",
+            "accept": "application/json"
+        },
+        params = {
+            "place": PLACE,
+            "time": TIME,
+            "way": WAY
+        }
+    )
+    assert response.status_code == 200, response.text
+
+def test_get_all_suggest(test_client):
+    response = test_client.post(
+        "/token/",
+        data={"username": EMAIL, "password": PASSWORD},
+    )
+    assert response.status_code == 200, response.text
+    token = response.json()["access_token"]  
+
+    response = test_client.get(
+        "/get_all_suggest/",
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {token}",
+            "accept": "application/json"
+        }
+    )
+    assert response.status_code == 200, response.text
