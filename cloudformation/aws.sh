798fd805f9c985ed624145b7066de1061d202d31
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-06-02 15:02:32 +0900

    リファクタ

--- cloudformation/aws.sh
+++ cloudformation/aws.sh
@@ -12,6 +12,6 @@ aws ecs update-service --cluster odekakekun-cluster --service odekakekun-fronten
 
 
 # 指定タスクの中に入る
-aws ecs execute-command --cluster odekakekun-cluster --task 192720e1d8b84bb7b893e00af396c3f7 --container odekakekun-frontend-container --interactive --command "/bin/sh"
-
+aws ecs execute-command --cluster odekakekun-cluster --task 62f8c08e8cf24977a1969c503cce5b8d --container odekakekun-frontend-container --interactive --command "/bin/sh"
+aws ecs execute-command --cluster odekakekun-cluster --task 6e62c7ce65de441e93578fbf9a85542c --container odekakekun-backend-container --interactive --command "/bin/sh"
 
