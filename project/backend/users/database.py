dc03d989e4c9edeea7691f5bccabe1f52db42e52
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-06-04 15:18:34 +0900

    DEPLOYMENT_STAGEを使って接続先DB_URLを変更

--- project/backend/users/database.py
+++ project/backend/users/database.py
@@ -1,11 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 # TODO appからみた相対パスになってるのでposix pathに直す
-DATABASE_URL = "sqlite:///./db/user.db"
+# ASK コンテナで管理している時もpytestで仮のDBを作るのは自分のproject内部でよいか -> よいはず
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread" : False})
+conf ={
+    'host':"anonymousendpoint.amazonaws.com",
+    'port':'5432',
+    'database':"DBname",
+    'user':"Project",
+    'password':"secret"
+}
+
+DEPLOYMENT_STAGE = os.getenv("DEPLOYMENT_STAGE")
+
+if DEPLOYMENT_STAGE == "production" or "test" :
+    engine = create_engine("sqlite:///./db/user.db", connect_args={"check_same_thread" : False})
+elif DEPLOYMENT_STAGE == "deployment":
+    engine = create_engine("postgresql://{user}:{password}@{host}:{port}/{database}".format(**conf), connect_args={"check_same_thread" : False})
 
 sessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit = False)
 
