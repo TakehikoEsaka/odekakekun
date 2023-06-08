27b72018e41bfd08f4bbb4222ba5c962178ca52b
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-06-08 19:47:07 +0900

    docker-composeで起動出来るように変更

--- project/backend/users/database.py
+++ project/backend/users/database.py
@@ -10,22 +10,23 @@ import os
 # 参考https://repost.aws/ja/knowledge-center/ecs-fargate-task-database-connection
 
 DEPLOYMENT_STAGE = os.getenv("DEPLOYMENT_STAGE")
-DB_USER = os.getenv("DBINFO").username
-DB_PASSWORD = os.getenv("DBINFO").password
-
-# TODO backendからdatabaseにテストデータを入れるスクリプトを作成して実行する
-conf ={
-    'host':"anonymousendpoint.amazonaws.com",
-    'port':'5432',
-    'database':"odekakekundb",
-    'user':DB_USER,
-    'password':DB_PASSWORD
-}
 
 if DEPLOYMENT_STAGE == "production" or "test" :
     engine = create_engine("sqlite:///./db/user.db", connect_args={"check_same_thread" : False})
+
 elif DEPLOYMENT_STAGE == "deployment":
     engine = create_engine("postgresql://{user}:{password}@{host}:{port}/{database}".format(**conf), connect_args={"check_same_thread" : False})
+    DB_USER = os.getenv("DBINFO").username
+    DB_PASSWORD = os.getenv("DBINFO").password
+
+    # TODO backendからdatabaseにテストデータを入れるスクリプトを作成して実行する
+    conf ={
+        'host':"anonymousendpoint.amazonaws.com",
+        'port':'5432',
+        'database':"odekakekundb",
+        'user':DB_USER,
+        'password':DB_PASSWORD
+    }
 
 sessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit = False)
 
