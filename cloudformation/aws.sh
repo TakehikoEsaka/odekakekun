d66fbdc677fba2f1d15edc67d090a69c5925002a
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-06-09 20:48:01 +0900

    code pipelineの準備

--- cloudformation/aws.sh
+++ cloudformation/aws.sh
@@ -11,6 +11,9 @@ aws ecs describe-tasks --cluster odekakekun-cluster --tasks fbfef898bcb44a74bf22
 aws ecs update-service --cluster odekakekun-cluster --service odekakekun-frontend-service-3 --task-definition odekakekun-backend-task:1 --desired-count 1 --force-new-deployment --enable-execute-command
 
 # 指定タスクの中に入る
-aws ecs execute-command --cluster odekakekun-cluster --task fbfef898bcb44a74bf229930aff5e226 --container odekakekun-frontend-container --interactive --command "/bin/sh"
-aws ecs execute-command --cluster odekakekun-cluster --task fbfef898bcb44a74bf229930aff5e226 --container odekakekun-backend-container --interactive --command "/bin/sh"
+aws ecs execute-command --cluster odekakekun-cluster --task b1e8f3045b774c94ba5930e8f22eeadf --container odekakekun-frontend-container --interactive --command "/bin/sh"
+aws ecs execute-command --cluster odekakekun-cluster --task 2cfc469f241842539cf78e5130d8d417 --container odekakekun-backend-container --interactive --command "/bin/sh"
+
+# サービス検出できているか確認
+dig +short service-discovery.odekakekun-network
 
