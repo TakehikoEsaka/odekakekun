8dbb7a2d2d35bae914d5b3547545624e056601b1
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-06-10 20:19:46 +0900

    baseURL変更

--- project/frontend/src/axios.tsx
+++ project/frontend/src/axios.tsx
@@ -3,7 +3,8 @@ import Axios from "axios";
 let baseURL;
 
 if (process.env.REACT_APP_DEPLOYMENT_STAGE === "production") {
-  baseURL = "http://service-discovery.odekakekun-network:80";
+  baseURL =
+    "http://internet-alb-532786072.ap-northeast-1.elb.amazonaws.com/api";
 }
 
 // docker-networkを利用している場合でもブラウザは名前解決が出来ないのでlocalhostでアクセスしておく
@@ -11,11 +12,11 @@ if (
   process.env.REACT_APP_DEPLOYMENT_STAGE === "development" ||
   process.env.REACT_APP_DEPLOYMENT_STAGE === "test"
 ) {
-  baseURL = "http://localhost:8080";
+  baseURL = "http://localhost:3000/api";
 }
 
 const instance = Axios.create({
-  baseURL: baseURL,
+  baseURL: "http://localhost:3000/api",
 });
 
 export default instance;
