f99e8bf87f43ce724695610c355439a75fc34d1b
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-06-11 17:27:08 +0900

    proxy周り追加

--- project/frontend/src/axios.tsx
+++ project/frontend/src/axios.tsx
@@ -1,10 +1,10 @@
 import Axios from "axios";
 
 let baseURL;
+const AlbEndpoint = process.env.REACT_APP_ELB_ENDPOINT;
 
 if (process.env.REACT_APP_DEPLOYMENT_STAGE === "production") {
-  baseURL =
-    "http://internet-alb-532786072.ap-northeast-1.elb.amazonaws.com/api";
+  baseURL = AlbEndpoint;
 }
 
 // docker-networkを利用している場合でもブラウザは名前解決が出来ないのでlocalhostでアクセスしておく
@@ -15,8 +15,10 @@ if (
   baseURL = "http://localhost:3000/api";
 }
 
+baseURL = "http://localhost:3000";
+
 const instance = Axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: baseURL,
 });
 
 export default instance;
