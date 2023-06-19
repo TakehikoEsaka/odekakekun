63ec9fd11a26a02294f42f59bb4520cf6cd35f30
TakehikoEsaka <take.pbfzbf302@gmail.com>
2023-06-19 17:27:53 +0900

    proxyをreactではなくnginx側でもつ

--- project/frontend/src/axios.tsx
+++ project/frontend/src/axios.tsx
@@ -1,26 +1,9 @@
 import Axios from "axios";
 
-let baseURL;
-const AlbEndpoint = process.env.REACT_APP_ELB_ENDPOINT;
-
-if (process.env.REACT_APP_DEPLOYMENT_STAGE === "production") {
-  baseURL = AlbEndpoint + "/api";
-}
-
-// docker-networkを利用している場合でもブラウザは名前解決が出来ないのでlocalhostでアクセスしておく
-if (
-  process.env.REACT_APP_DEPLOYMENT_STAGE === "development" ||
-  process.env.REACT_APP_DEPLOYMENT_STAGE === "test"
-) {
-  baseURL = "http://localhost:80/api";
-}
-
-if (!process.env.REACT_APP_DEPLOYMENT_STAGE) {
-  baseURL = "localhost:80/api";
-}
-
 const instance = Axios.create({
-  baseURL: baseURL,
+  baseURL: process.env.REACT_APP_API_ENDPOINT + "/api",
 });
 
+console.log("base URL is", process.env.REACT_APP_API_ENDPOINT + "/api");
+console.log("target URL is", process.env.REACT_APP_PROXY_ENDPOINT);
 export default instance;
